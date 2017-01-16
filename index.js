360261ac9c3f2efa7682ba62c7902e92c0bf05b8
limichange <limichange@hotmail.com>
2017-01-16 21:34:32 +0800

    🚧  - Work in progress.

---
+++ index.js
@@ -0,0 +1,28 @@
+window.alert = function(name) {
+  var iframe = document.createElement("IFRAME")
+  iframe.style.display = "none"
+  iframe.setAttribute("src", 'data:text/plain,')
+  document.documentElement.appendChild(iframe)
+  window.frames[0].window.alert(name)
+  iframe.parentNode.removeChild(iframe)
+}
+
+window.confirm = function (name) {
+  var iframe = document.createElement("IFRAME")
+  iframe.style.display = "none"
+  iframe.setAttribute("src", 'data:text/plain,')
+  document.documentElement.appendChild(iframe)
+  mframe = window.frames[0].window.confirm(name)
+  iframe.parentNode.removeChild(iframe)
+  return mframe
+}
+
+window.prompt = function (name) {
+  var iframe = document.createElement("IFRAME")
+  iframe.style.display = "none"
+  iframe.setAttribute("src", 'data:text/plain,')
+  document.documentElement.appendChild(iframe)
+  mframe = window.frames[0].window.prompt(name)
+  iframe.parentNode.removeChild(iframe)
+  return mframe
+}
